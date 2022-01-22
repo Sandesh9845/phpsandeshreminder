35c94323cce2a823deee68c0227cc86b03bec03f
sanish khadka <rickeykhd@gmail.com>
2022-01-22 16:40:00 +1100

    reminder frontend done

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="index.style.css">
+</head>
+<body>
+    <!-- header -->
+<div>
+    <div class="loginlogout">
+    <img src="./img/logo.PNG" alt="">
+    <div class="tl" >
+    <a  class="br" href="">signup</a>
+    <a class="br" href="">login</a>
+
+    </div>
+    
+    </div>
+    
+        <ul>
+            <li> <a href="">Dashboard</a></li>
+            <li> <a href="">Manage client</a></li>
+            <li> <a href="">Add Clients</a></li>
+            <li> <a href="">Sort Clients</a></li>
+        </ul>
+       
+    </div>
+        </div>
+
+<!-- reminder -->
+<section class="reminder">
+    <div>
+        <h1>Reminder</h1>
+    <div class="R1">
+             <div class="r1s"> 
+                 <div class="R11">Today</div>     
+                <div class="R12">schedule</div>  
+            </div>
+             <div class="r1ss">
+                 <div class="R13">All</div>     
+                <div class="R14">Flag</div>  
+             </div>
+                  
+    </div>       
+            
+                
+             </div>  
+            <div>
+                <h1>Activities Today</h1>
+            <div class="R2">
+                <div class="R21"></div>     
+                <div class="R22"></div>      
+             </div>  
+            </div>
+                    
+</div>
+                
+                    
+                
+                    
+                    
+                
+            
+            </section>
+
+
+
+
+
+
+</body>
+</html>
